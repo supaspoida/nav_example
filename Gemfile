e5609b4aed95934981f286fda8318a60c36c908b
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2011-10-06 22:10:58 -0400

    rails new ../nav_example -O -T
    * No ActiveRecord
    * No TestUnit
    * Use haml

--- Gemfile
+++ Gemfile
@@ -1,3 +1,13 @@
 source :rubygems
 
 gem 'rails', '3.1.1.rc3'
+
+gem 'haml-rails'
+
+group :assets do
+  gem 'sass-rails', '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NavExample::Application.config.secret_token = '05a2625ff0e3b3c3fb9eaf91b993303217ae7438f020a7a87912ef2656646d59543059285164cd47f1277b93bf058c829b51458f62b11cceef05ffc84e027a06'
